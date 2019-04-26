aee3d96a6decb27f32b54b783ac1b8f2ac5d0c44
Gold Venus <jostar0610@gmail.com>
2019-04-26 20:19:34 +0300

    mobile responsive navbar

--- src/components/header.js
+++ src/components/header.js
@@ -3,57 +3,97 @@ import PropTypes from "prop-types"
 import React from "react"
 import { Container, Nav, NavItem } from "react-bootstrap"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Container className="container" style={{
-      margin: `0 auto`,
-      maxWidth: 1280,
-      padding: `1.45rem 1.0875rem 0 1.0875rem`}}>
-        <Nav className = "header_nav_menu">
-          <div className="col-3">
-            <Link  to='/' style={{
+class Header extends React.Component {
+  render () {
+    this.state = {
+      mobile_state: false
+    }
+    const changeview = () =>{
+      console.log("View Change", this.state.mobile_state);
+      this.state.mobile_state = !this.state.mobile_state;
+    }
+    return (
+      <header>
+        <Container className="container" style={{
+          margin: `0 auto`,
+          maxWidth: 1280,
+          padding: `1.45rem 1.0875rem 0 1.0875rem` }}>
+          <Nav className="header_nav_menu">
+            <div className="col-3">
+              <Link to='/' style={{
                 color: 'black',
                 textDecoration: 'none'}}>
-                <img src={require('../images/Logo.svg')} style={{width:120,height:60,paddingBottom:0,marginBottom:0,pointerEvents:'auto'}}/>
-            </Link>
-          </div>
-          <div className="col-9 d-lg-flex d-none" style={{'display' : `flex`}}>
-            <Link to='/about/'>
-              <NavItem eventKey={1}>
-                <div>About</div>
-              </NavItem>
-            </Link>
-            <Link to='/partnering/'>
-              <NavItem eventKey={2}>
-                <div>Partnering</div>
-              </NavItem>
-            </Link>
-            <Link to='/chemistry/'>
-              <NavItem eventKey={3}>
-                <div>Chemistry of Polymers</div>
-              </NavItem>
-            </Link>
-            <Link to='/nature/'>
-              <NavItem eventKey={4}>
-                <div>From Nature to Lab</div>
-              </NavItem>
-            </Link>
-          </div>
-          <div className="d-none d-sm-flex">
-            Venus
-          </div>
+                <img src={require('../images/Logo.svg')}
+                     style={{ width: 120, height: 60, paddingBottom: 0, marginBottom: 0, pointerEvents: 'auto' }}/>
+              </Link>
+            </div>
+            <div className="col-9 d-lg-flex d-none" style={{ 'display': `flex` }}>
+              <Link to='/about/'>
+                <NavItem eventKey={1}>
+                  <div>About</div>
+                </NavItem>
+              </Link>
+              <Link to='/partnering/'>
+                <NavItem eventKey={2}>
+                  <div>Partnering</div>
+                </NavItem>
+              </Link>
+              <Link to='/chemistry/'>
+                <NavItem eventKey={3}>
+                  <div>Chemistry of Polymers</div>
+                </NavItem>
+              </Link>
+              <Link to='/nature/'>
+                <NavItem eventKey={4}>
+                  <div>From Nature to Lab</div>
+                </NavItem>
+              </Link>
+            </div>
+            <div className="mobile-header-menu col-9" onClick={changeview}>
+              <img src={require('../images/menu-lines.svg')} onClick={changeview}/>
+            </div>
           </Nav>
-    </Container>
+        </Container>
+        {
+          this.state.mobile_state===true ?
+            <div style={{
+              'width': `100%`,
+              'height': `100%`,
+              'position': `fixed`,
+              'display': `block`,
+              'background-color': `white`,
+              'z-index': '1'
+            }}>
+              <Link to='/about/'>
+                <NavItem eventKey={1}>
+                  <div>About</div>
+                </NavItem>
+              </Link>
+              <Link to='/partnering/'>
+                <NavItem eventKey={2}>
+                  <div>Partnering</div>
+                </NavItem>
+              </Link>
+              <Link to='/chemistry/'>
+                <NavItem eventKey={3}>
+                  <div>Chemistry of Polymers</div>
+                </NavItem>
+              </Link>
+              <Link to='/nature/'>
+                <NavItem eventKey={4}>
+                  <div>From Nature to Lab</div>
+                </NavItem>
+              </Link>
+            </div>
+            :
+            null
+        }
 
-  </header>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+      </header>
+    );
+  }
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
-export default Header
+export default Header;
